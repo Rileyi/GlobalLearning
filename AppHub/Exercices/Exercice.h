48b6037ea750cdf77c76125354717c1986e0b565
Laurent <lau.cazanove@gmail.com>
2016-01-24 19:56:50 +0100

    Exercice load, general logic

---
+++ AppHub/Exercices/Exercice.h
@@ -0,0 +1,45 @@
+#ifndef EXERCICE_H
+#define EXERCICE_H
+
+#pragma GCC diagnostic ignored "-Weffc++" // disable Weffc++ warnings
+#include "tinyxml2.h"
+#pragma GCC diagnostic pop // turn the warnings back on
+
+#include <iostream>
+
+class Exercice
+{
+    // Public enums
+    public:
+        enum class Type
+        {
+            None, DotToDot
+        };
+
+    // Class attributes
+    protected:
+        static const std::string XML_ROOT_ELEMENT;
+        static const std::string XML_TYPE_ATTRIBUTE;
+        static const std::string XML_REQUIREMENTS_ELEMENT;
+
+    // Methods
+    public:
+        Exercice();
+        virtual ~Exercice();
+        Exercice(const Exercice& other);
+        Exercice& operator=(const Exercice& other);
+
+        bool loadFromFile(const std::string &path);
+
+    protected:
+        bool setType(const std::string &type);
+        bool loadRequirements(const tinyxml2::XMLElement *element);
+        bool loadContent(const tinyxml2::XMLDocument &document);
+        bool loadDotToDotContent(const tinyxml2::XMLDocument &document);
+
+    // Attributes
+    protected:
+        Exercice::Type _type;
+};
+
+#endif // EXERCICE_H
--- AppHub/Exercices/main.cpp
+++ AppHub/Exercices/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
+#include "Exercice.h"
+
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Exercice ex;
+
+    if (ex.loadFromFile("butterfly.jpg.dots.xml"))
+        cout << "Loading succeeded" << endl;
+    else
+        cout << "Loading failed" << endl;
+
     return 0;
 }
